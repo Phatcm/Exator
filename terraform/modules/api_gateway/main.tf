54e628763b9397de16e0409fde61e38b011774d5
cmpdev <phat.caominh.dev@gmail.com>
2024-01-11 14:24:50 +0700

    test

--- terraform/modules/api_gateway/main.tf
+++ terraform/modules/api_gateway/main.tf
@@ -11,7 +11,7 @@ resource "aws_api_gateway_deployment" "prod" {
     rest_api_id = aws_api_gateway_rest_api.api_gateway.id
 
     triggers = {
-    redeployment = sha1(
+        redeployment = sha1(
             jsonencode([
                 file("modules/api_gateway/main.tf"), 
                 file("modules/api_gateway/resource_health.tf"), 
@@ -22,6 +22,7 @@ resource "aws_api_gateway_deployment" "prod" {
             ])
         )
     }
+    
     lifecycle {
         create_before_destroy = true
     }
--- terraform/modules/api_gateway/resource_question.tf
+++ terraform/modules/api_gateway/resource_question.tf
@@ -68,3 +68,54 @@ resource "aws_api_gateway_integration" "delete_question_integration" {
     type                    = "AWS_PROXY"
     uri                     = var.lambda_invoke_arn
 }
+
+#Options_question
+resource "aws_api_gateway_method" "options_question" {
+    rest_api_id   = aws_api_gateway_rest_api.api_gateway.id
+    resource_id   = aws_api_gateway_resource.question.id
+    http_method   = "OPTIONS"
+    authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "options_question" {
+    rest_api_id             = aws_api_gateway_rest_api.api_gateway.id
+    resource_id             = aws_api_gateway_resource.question.id
+    http_method             = aws_api_gateway_method.options_question.http_method
+    integration_http_method = "OPTIONS"
+    type                    = "MOCK"
+    request_templates = {
+        "application/json" = "{\"statusCode\": 200}"
+    }
+}
+
+resource "aws_api_gateway_method_response" "options_question" {
+  rest_api_id = aws_api_gateway_rest_api.api_gateway.id
+  resource_id = aws_api_gateway_resource.question.id
+  http_method = aws_api_gateway_method.options_question.http_method
+  status_code = "200"
+
+  //cors section
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = true,
+    "method.response.header.Access-Control-Allow-Methods" = true,
+    "method.response.header.Access-Control-Allow-Origin" = true
+  }
+}
+
+resource "aws_api_gateway_integration_response" "options_question" {
+  rest_api_id = aws_api_gateway_rest_api.api_gateway.id
+  resource_id = aws_api_gateway_resource.question.id
+  http_method = aws_api_gateway_method.options_question.http_method
+  status_code = aws_api_gateway_method_response.options_question.status_code
+
+  //cors
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" =  "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
+    "method.response.header.Access-Control-Allow-Methods" = "'GET,OPTIONS,POST,PUT,DELETE'",
+    "method.response.header.Access-Control-Allow-Origin" = "'*'"
+  }
+  depends_on = [
+    aws_api_gateway_method.options_question,
+    aws_api_gateway_integration.options_question,
+  ]
+}
