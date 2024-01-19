f5451ad1c18d086ca10d361ddbb859f0921c0453
kham <ckiet1201@gmail.com>
2024-01-19 08:58:36 +0700

    a

--- resources/custom_encoder.py
+++ resources/custom_encoder.py
@@ -4,7 +4,6 @@ from decimal import Decimal
 class CustomEncoder(json.JSONEncoder):
     def default(self, obj):
         if isinstance(obj, Decimal):
-
             return float(obj)
         return json.JSONEncoder.default(self, obj)
     
