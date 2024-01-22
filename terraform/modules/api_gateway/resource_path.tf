7c65307551ea4217c3ff85bdf1fd46d9dbd98bf3
cmpdev <phat.caominh.dev@gmail.com>
2024-01-22 10:20:48 +0700

    history resource changes

--- terraform/modules/api_gateway/resource_path.tf
+++ terraform/modules/api_gateway/resource_path.tf
@@ -90,7 +90,7 @@ module "resource_history" {
 # /history_attempts
 module "resource_history_attempts" {
     source = "../api_gateway_resource"
-    path_part = "history_attempts"
+    path_part = "attempts"
     parent_id = module.resource_history.resource_id
     rest_api_id = aws_api_gateway_rest_api.api_gateway.id
     lambda_invoke_arn = var.lambda_invoke_arn
@@ -100,7 +100,7 @@ module "resource_history_attempts" {
 # /history_questions
 module "resource_history_questions" {
     source = "../api_gateway_resource"
-    path_part = "history_questions"
+    path_part = "questions"
     parent_id = module.resource_history.resource_id
     rest_api_id = aws_api_gateway_rest_api.api_gateway.id
     lambda_invoke_arn = var.lambda_invoke_arn
