f3adeccf58d9cb7ce2e0107535035a0271e68a36
cmpdev <phat.caominh.dev@gmail.com>
2024-01-22 13:49:48 +0700

    update lambda main.tf

--- terraform/modules/lambda/main.tf
+++ terraform/modules/lambda/main.tf
@@ -26,14 +26,13 @@ resource "aws_lambda_function" "lambda_function" {
   source_code_hash = data.archive_file.lambda_zip.output_base64sha256
   runtime          = var.lambda_runtime
 
-  environment {
-    layers = [ aws_lambda_layer_version.lambda_layer.arn ]
-    depends_on = [ aws_lambda_layer_version.lambda_layer ]
+  layers = [ aws_lambda_layer_version.lambda_layer.arn ]
+  depends_on = [ aws_lambda_layer_version.lambda_layer ]
 
-    timeout = 500
-    environment {
+  timeout = 500
+  environment {
     variables = {
-      S3_BUCKET_NAME = var.s3_bucket_name
+      #S3_BUCKET_NAME = var.s3_bucket_name
       QUESTIONS_TABLE_NAME = var.questions_table_name
       ATTEMPTS_TABLE_NAME  = var.attempts_table_name
     }
